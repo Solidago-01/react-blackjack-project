5800744e8c03686d15fc375473282bd3266b90fa
Autumn <l.a.gnadinger@gmail.com>
2024-12-16 15:21:34 -0500

    add: repo link in about

--- src/About.jsx
+++ src/About.jsx
@@ -12,10 +12,10 @@ function About ({aboutVisibility, closeAbout}) {
                 This app is for demonstration purposes only. Showcasing use of React, Vite, & state handling workflows. 
                 <br></br>
                 <br></br>
-                See the GitHub repo for this project here.
+                <a href="https://github.com/Solidago-01/react-blackjack-project" target='_blank'>See the GitHub repo for this project here.</a></p>
                 <br></br>
                 <br></br>
-                <a className='portfolioLink' href="https://lautumngnadinger.com/" target='_blank'>L Autumn Gnadinger</a></p>
+                <a className='portfolioLink' href="https://lautumngnadinger.com/" target='_blank'>L Autumn Gnadinger</a>
                 <button onClick={closeAbout} className={aboutVisibility}>close</button>
             </div>
         </>
