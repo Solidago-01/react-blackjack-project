73abe2694a8f7782fb144d12e64f94853e203f43
Autumn <l.a.gnadinger@gmail.com>
2024-12-16 11:50:08 -0500

    move image to public

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -4,313 +4,313 @@ export const Deck = [
 
         { 
             id: 1,
-            src: "src/assets/cards/2H.svg",
+            src: "public/assets/cards/2H.svg",
             name: "Two of Hearts",
             value: 2  
         },
         { 
             id: 2,
-            src: "src/assets/cards/2D.svg",
+            src: "public/assets/cards/2D.svg",
             name: "Two of Diamonds",
             value: 2 
         },
         { 
             id: 3,
-            src: "src/assets/cards/2C.svg",
+            src: "public/assets/cards/2C.svg",
             name: "Two of Clubs",
             value: 2 
         },
         { 
             id: 4,
-            src: "src/assets/cards/2S.svg",
+            src: "public/assets/cards/2S.svg",
             name: "Two of Spades",
             value: 2 
         },
         { 
             id: 5,
-            src: "src/assets/cards/3H.svg",
+            src: "public/assets/cards/3H.svg",
             name: "Three of Hearts",
             value: 3 
         },
         { 
             id: 6,
-            src: "src/assets/cards/3D.svg",
+            src: "public/assets/cards/3D.svg",
             name: "Three of Diamonds",
             value: 3 
         },
         { 
             id: 7,
-            src: "src/assets/cards/3C.svg",
+            src: "public/assets/cards/3C.svg",
             name: "Three of Clubs",
             value: 3 
         },
         { 
             id: 8,
-            src: "src/assets/cards/3S.svg",
+            src: "public/assets/cards/3S.svg",
             name: "Three of Spades",
             value: 3 
         },
         { 
             id: 9,
-            src: "src/assets/cards/4H.svg",
+            src: "public/assets/cards/4H.svg",
             name: "Four of Hearts",
             value: 4 
         },
         { 
             id: 10,
-            src: "src/assets/cards/4D.svg",
+            src: "public/assets/cards/4D.svg",
             name: "Four of Diamonds",
             value: 4 
         },
         { 
             id: 11,
-            src: "src/assets/cards/4C.svg",
+            src: "public/assets/cards/4C.svg",
             name: "Four of Clubs",
             value: 4 
         },
         { 
             id: 12,
-            src: "src/assets/cards/4S.svg",
+            src: "public/assets/cards/4S.svg",
             name: "Four of Spades",
             value: 4 
         },
         { 
             id: 13,
-            src: "src/assets/cards/5H.svg",
+            src: "public/assets/cards/5H.svg",
             name: "Five of Hearts",
             value: 5 
         },
         { 
             id: 14,
-            src: "src/assets/cards/5D.svg",
+            src: "public/assets/cards/5D.svg",
             name: "Five of Diamonds",
             value: 5 
         },
         { 
             id: 15,
-            src: "src/assets/cards/5C.svg",
+            src: "public/assets/cards/5C.svg",
             name: "Five of Clubs",
             value: 5 
         },
         { 
             id: 16,
-            src: "src/assets/cards/5S.svg",
+            src: "public/assets/cards/5S.svg",
             name: "Five of Spades",
             value: 5 
         },
         { 
             id: 17,
-            src: "src/assets/cards/6H.svg",
+            src: "public/assets/cards/6H.svg",
             name: "Six of Hearts",
             value: 6 
         },
         { 
             id: 18,
-            src: "src/assets/cards/6D.svg",
+            src: "public/assets/cards/6D.svg",
             name: "Six of Diamonds",
             value: 6 
         },
         { 
             id: 19,
-            src: "src/assets/cards/6C.svg",
+            src: "public/assets/cards/6C.svg",
             name: "Six of Clubs",
             value: 6 
         },
         { 
             id: 20,
-            src: "src/assets/cards/6S.svg",
+            src: "public/assets/cards/6S.svg",
             name: "Six of Spades",
             value: 6 
         },
         { 
             id: 21,
-            src: "src/assets/cards/7H.svg",
+            src: "public/assets/cards/7H.svg",
             name: "Seven of Hearts",
             value: 7 
         },
         { 
             id: 22,
-            src: "src/assets/cards/7D.svg",
+            src: "public/assets/cards/7D.svg",
             name: "Seven of Diamonds",
             value: 7 
         },
         { 
             id: 23,
-            src: "src/assets/cards/7C.svg",
+            src: "public/assets/cards/7C.svg",
             name: "Seven of Clubs",
             value: 7 
         },
         { 
             id: 24,
-            src: "src/assets/cards/7S.svg",
+            src: "public/assets/cards/7S.svg",
             name: "Seven of Spades",
             value: 7 
         },
         { 
             id: 25,
-            src: "src/assets/cards/8H.svg",
+            src: "public/assets/cards/8H.svg",
             name: "Eight of Hearts",
             value: 8 
         },
         { 
             id: 26,
-            src: "src/assets/cards/8D.svg",
+            src: "public/assets/cards/8D.svg",
             name: "Eight of Diamonds",
             value: 8  
         },
         { 
             id: 27,
-            src: "src/assets/cards/8C.svg",
+            src: "public/assets/cards/8C.svg",
             name: "Eight of Clubs",
             value: 8  
         },
         { 
             id: 28,
-            src: "src/assets/cards/8S.svg",
+            src: "public/assets/cards/8S.svg",
             name: "Eight of Spades",
             value: 8  
         },
         { 
             id: 29,
-            src: "src/assets/cards/9H.svg",
+            src: "public/assets/cards/9H.svg",
             name: "Nine of Hearts",
             value: 9 
         },
         { 
             id: 30,
-            src: "src/assets/cards/9D.svg",
+            src: "public/assets/cards/9D.svg",
             name: "Nine of Diamonds",
             value: 9 
         },
         { 
             id: 31,
-            src: "src/assets/cards/9C.svg",
+            src: "public/assets/cards/9C.svg",
             name: "Nine of Clubs",
             value: 9 
         },
         { 
             id: 32,
-            src: "src/assets/cards/9S.svg",
+            src: "public/assets/cards/9S.svg",
             name: "Nine of Spades",
             value: 9 
         },
         { 
             id: 33,
-            src: "src/assets/cards/10H.svg",
+            src: "public/assets/cards/10H.svg",
             name: "Ten of Hearts",
             value: 10 
         },
         { 
             id: 34,
-            src: "src/assets/cards/10D.svg",
+            src: "public/assets/cards/10D.svg",
             name: "Ten of Diamonds",
             value: 10
         },
         { 
             id: 35,
-            src: "src/assets/cards/10C.svg",
+            src: "public/assets/cards/10C.svg",
             name: "Ten of Clubs",
             value: 10 
         },
         { 
             id: 36,
-            src: "src/assets/cards/10S.svg",
+            src: "public/assets/cards/10S.svg",
             name: "Ten of Spades",
             value: 10 
         },
         { 
             id: 37,
-            src: "src/assets/cards/JH.svg",
+            src: "public/assets/cards/JH.svg",
             name: "Jack of Hearts",
             value: 10 
         },
         { 
             id: 38,
-            src: "src/assets/cards/JD.svg",
+            src: "public/assets/cards/JD.svg",
             name: "Jack of Diamonds",
             value: 10 
         },
         { 
             id: 39,
-            src: "src/assets/cards/JC.svg",
+            src: "public/assets/cards/JC.svg",
             name: "Jack of Clubs",
             value: 10 
         },
         { 
             id: 40,
-            src: "src/assets/cards/JS.svg",
+            src: "public/assets/cards/JS.svg",
             name: "Jack of Spades",
             value: 10 
         },
         { 
             id: 41,
-            src: "src/assets/cards/QH.svg",
+            src: "public/assets/cards/QH.svg",
             name: "Queen of Hearts",
             value: 10 
         },
         { 
             id: 42,
-            src: "src/assets/cards/QD.svg",
+            src: "public/assets/cards/QD.svg",
             name: "Queen of Diamonds",
             value: 10 
         },
         { 
             id: 43,
-            src: "src/assets/cards/QC.svg",
+            src: "public/assets/cards/QC.svg",
             name: "Queen of Clubs",
             value: 10 
         },
         { 
             id: 44,
-            src: "src/assets/cards/QS.svg",
+            src: "public/assets/cards/QS.svg",
             name: "Queen of Spades",
             value: 10 
         },
         { 
             id: 45,
-            src: "src/assets/cards/KH.svg",
+            src: "public/assets/cards/KH.svg",
             name: "King of Hearts",
             value: 10 
         },
         { 
             id: 46,
-            src: "src/assets/cards/KD.svg",
+            src: "public/assets/cards/KD.svg",
             name: "King of Diamonds",
             value: 10 
         },
         { 
             id: 47,
-            src: "src/assets/cards/KC.svg",
+            src: "public/assets/cards/KC.svg",
             name: "King of Clubs",
             value: 10 
         },
         { 
             id: 48,
-            src: "src/assets/cards/KS.svg",
+            src: "public/assets/cards/KS.svg",
             name: "King of Spades",
             value: 10 
         },
         { 
             id: 49,
-            src: "src/assets/cards/AH.svg",
+            src: "public/assets/cards/AH.svg",
             name: "Ace of Hearts",
             value: 11 
         },
         { 
             id: 50,
-            src: "src/assets/cards/AD.svg",
+            src: "public/assets/cards/AD.svg",
             name: "Ace of Diamonds",
             value: 11 
         },
         { 
             id: 51,
-            src: "src/assets/cards/AC.svg",
+            src: "public/assets/cards/AC.svg",
             name: "Ace of Clubs",
             value: 11 
         },
         { 
             id: 52,
-            src: "src/assets/cards/AS.svg",
+            src: "public/assets/cards/AS.svg",
             name: "Ace of Spades",
             value: 11 
         }
