a4b471543c9903be07cb4a055524ebb173164ae4
Autumn <l.a.gnadinger@gmail.com>
2024-12-16 15:24:37 -0500

    fix: repo link style in about

--- src/About.jsx
+++ src/About.jsx
@@ -9,13 +9,13 @@ function About ({aboutVisibility, closeAbout}) {
             <div className={aboutClasses}>
                 <h2 className={aboutVisibility}>About this App</h2>
                 <p className={aboutVisibility}>
-                This app is for demonstration purposes only. Showcasing use of React, Vite, & state handling workflows. 
+                This app is for demonstration purposes only. Showcasing use of React, Vite, deployment, & state handling workflows. 
                 <br></br>
                 <br></br>
-                <a href="https://github.com/Solidago-01/react-blackjack-project" target='_blank'>See the GitHub repo for this project here.</a></p>
+                <a href="https://github.com/Solidago-01/react-blackjack-project" target='_blank'>See the GitHub repo for this project here.</a>
                 <br></br>
                 <br></br>
-                <a className='portfolioLink' href="https://lautumngnadinger.com/" target='_blank'>L Autumn Gnadinger</a>
+                <a className='portfolioLink' href="https://lautumngnadinger.com/" target='_blank'>L Autumn Gnadinger</a></p>
                 <button onClick={closeAbout} className={aboutVisibility}>close</button>
             </div>
         </>
